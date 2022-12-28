6730f1a2fbf5dd38bd9e97b7b65aeaf8ca911f55
kyoohyun <kgh2120@naver.com>
2022-12-28 17:58:02 +0900

    MZRF-43 백그라운드 미디어쿼리 추가

--- src/components/GlobalLayout.tsx
+++ src/components/GlobalLayout.tsx
@@ -8,15 +8,19 @@ interface GlobalLayoutProps{
 
 const GlobalBody = styled.div`
   background-color: #002967;
+  position: relative;
   width: 500px;
   height: 100vh;
   margin: 0 auto;
+  
+  @media (max-width:500px){
+    width : 100%;
+  }
 `
 const GlobalBackground = styled.div`
   background-color: black;
   width: 100%;
 `
-
 export default function GlobalLayout({children}:GlobalLayoutProps) : JSX.Element{
     return <>
         <GlobalBackground>
