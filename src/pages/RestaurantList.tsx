70170982677246cb028d27903760309efc6bc990
kyoohyun <kgh2120@naver.com>
2022-12-30 17:25:47 +0900

    MZRF-28 카테고리 목록 조회 기능 추가

--- src/pages/RestaurantList.tsx
+++ src/pages/RestaurantList.tsx
@@ -1,9 +1,23 @@
 import RestaurantListTitle from "../components/RestaurantListTitle";
 import RestaurantListBody from "../components/RestaurantListBody";
+import {useLocation} from "react-router";
+import Category from "../models/category";
+
+
+const getLocationCategory = (): Category => {
+  const category = useLocation().state?.category as Category
+  if (category.name == '?' || !category.name) return {name: "ALL"} as Category
+  if(category.name == '양식 아시안') return {name: "양식/아시안"} as Category
+  if(category.name == '카페 디저트') return {name: "카페/디저트"} as Category
+  return category
+}
+
 
 export default function RestaurantList() {
+  const category = getLocationCategory()
+
   return <>
-    <RestaurantListTitle/>
-    <RestaurantListBody/>
+    <RestaurantListTitle _category={category}/>
+    <RestaurantListBody _category={category}/>
   </>
 }
