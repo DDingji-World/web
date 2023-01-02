5e6a88d6f73f04383d15740c33c6feef12f8e150
jinlee0 <wls4370@gmail.com>
2023-01-02 15:28:10 +0900

    MZRF-82 추첨 카드에 지도 뷰 추가

---
+++ src/components/KakaoMap.tsx
@@ -0,0 +1,30 @@
+import { useEffect } from 'react'
+import styled from 'styled-components'
+
+const Map = styled.div`
+  width: 100%;
+  height: 50%;
+  background-color: white;
+`
+
+interface KakaoMapProps {
+  x: number
+  y: number
+}
+export default function KakaoMap({ x, y }: KakaoMapProps) {
+  useEffect(() => {
+    const { kakao } = window
+    const mapDiv = document.querySelector('#map')
+    const markerPosition = new kakao.maps.LatLng(x, y)
+    const options = {
+      center: markerPosition,
+      level: 3,
+    }
+
+    const map = new kakao.maps.Map(mapDiv, options)
+    new kakao.maps.Marker({
+      position: markerPosition,
+    }).setMap(map)
+  }, [])
+  return <Map id="map" />
+}
