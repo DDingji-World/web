d11f8e4995cedf6c308f769f463715f4a970c117
kyoohyun <kgh2120@naver.com>
2023-01-03 13:46:22 +0900

    MZRF-75 PWA 적용하기

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import {VitePWA} from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    VitePWA({
+      injectRegister: 'inline',
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      }})],
   server: {
     proxy: {
       '/restaurant': 'http://3.36.253.255:8080',
